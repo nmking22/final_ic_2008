0967c28e944ab428624c694c4acd1a53ff6bb467
Nick King <nickmaxking@gmail.com>
2020-09-23 09:49:14 -0600

    Add ingredients method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -7,4 +7,12 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    all_ingredients = @recipes.map do |recipe|
+      recipe.ingredients.map do |ingredient|
+        ingredient
+      end
+    end.flatten.uniq
+  end
 end
