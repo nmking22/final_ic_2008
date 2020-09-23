686c2d2d5acbb7fd01118c9e9cf13c8f930766c3
Nick King <nickmaxking@gmail.com>
2020-09-23 09:57:10 -0600

    Add test for enough_ingredients_for? method

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ingredient'
 require './lib/pantry'
+require './lib/recipe'
 
 class PantryTest < Minitest::Test
   def test_it_exists_and_has_readable_attributes
@@ -29,7 +30,30 @@ class PantryTest < Minitest::Test
     assert_equal 15, pantry.stock_check(ingredient1)
 
     pantry.restock(ingredient2, 7)
-    
+
     assert_equal 7, pantry.stock_check(ingredient2)
+  end
+
+  def test_it_can_check_if_it_has_enough_ingredients_for_a_recipe
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+
+    assert_equal false, pantry.enough_ingredients_for?(recipe1)
+
+    pantry.restock(ingredient2, 7)
+
+    assert_equal false, pantry.enough_ingredients_for?(recipe1)
+
+    pantry.restock(ingredient2, 1)
+
+    assert_equal true, pantry.enough_ingredients_for?(recipe1)
   end
 end
