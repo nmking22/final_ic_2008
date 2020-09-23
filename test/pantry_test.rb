b0e02a670e5cec6b856b83ec1f175887f90ed06a
Nick King <nickmaxking@gmail.com>
2020-09-23 09:11:07 -0600

    Add pantry tests for initialize and readable attributes

---
+++ test/pantry_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def test_it_exists_and_has_readable_attributes
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+    assert_equal ({}), pantry.stock
+  end
+end
