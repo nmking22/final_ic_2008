1aa2a0b8f3c3cd88931e5ec9345558b10f34375f
Nick King <nickmaxking@gmail.com>
2020-09-23 10:54:23 -0600

    Add summary method - working  except for ingredient sorting

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -23,4 +23,23 @@ class CookBook
       recipe.total_calories
     end
   end
+
+  def summary
+    summary_array = @recipes.map do |recipe|
+      recipe_hash = Hash.new
+      recipe_hash[:name] = recipe.name
+      recipe_hash[:details] = Hash.new
+      recipe_hash[:details][:ingredients] = recipe.ingredients_required.map do |ingredient, quantity|
+        ingredient_hash = Hash.new
+        ingredient_hash[:ingredient] = ingredient.name
+        ingredient_hash[:amount] = "#{quantity} #{ingredient.unit}"
+        ingredient_hash
+      end
+      # recipe_hash[:details][:ingredients].sort_by! do |ingredient|
+      #   ingredient[:name]
+      # end.reverse
+      recipe_hash[:total_calories] = recipe.total_calories
+      recipe_hash
+    end
+  end
 end
