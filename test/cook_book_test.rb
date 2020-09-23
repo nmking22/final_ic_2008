78363c6b498fca7628dec493aad6cd78d8674793
Nick King <nickmaxking@gmail.com>
2020-09-23 09:45:00 -0600

    Add test for ingredients method

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -23,4 +23,27 @@ class CookBookTest < Minitest::Test
 
     assert_equal [recipe1, recipe2], cookbook.recipes
   end
+
+  def test_it_can_return_ingredients
+    pantry = Pantry.new
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2 = Recipe.new("Cheese Burger")
+
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    expected = [ingredient1, ingredient2, ingredient3, ingredient4]
+
+    assert_equal expected, cookbook.ingredients
+  end
 end
