9928ec6d7ecdcd3386a31d75e8dbd834531aabe6
Nick King <nickmaxking@gmail.com>
2020-09-23 10:19:07 -0600

    Add date attr_reader method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,7 +1,9 @@
 class CookBook
-  attr_reader :recipes
+  attr_reader :recipes,
+              :date
   def initialize
     @recipes = []
+    @date = Date.today.strftime("%m-%d-%Y")
   end
 
   def add_recipe(recipe)
